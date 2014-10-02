127e64880ede8c13e6c9ba014a76d983fc4b5cf2
jpleitao <jocaleitao93@gmail.com>
2014-10-02 18:44:24 +0100

    Changes to the test cases and new script. Now we only use 50 test cases

--- TP2/script.m
+++ TP2/script.m
@@ -10,53 +10,30 @@ load('PerfectArial.mat');
 cond = 1;
 Pfinal = [];
 Tfinal = [];
-drawing_order = [1:9,0];
 
-while cond < 11%Since cond starts in 1 and we want to make 500 iterations we need to run the cycle while cond<=10, that is while cond<11
-	% Open mpaper %
-	mpaper;
-	
-	% This wait for the Figure 1 - mpaper - to close %
-	waitfor(1);
+% Open mpaper %
+mpaper;
 
-	load('P.mat');
+% This wait for the Figure 1 - mpaper - to close %
+waitfor(1);
 
-	% Read the P array, to get the elements drawn, and sort their order
-	current_iteration = 0;
+load('P.mat');
 
-	while current_iteration < 5%We have 5 rows
-		
-		current_iteration2 = 0;
-		available_positions = [1:10];%All the possible positions for each row
+% Read the P array, to get the elements drawn, and sort their order
+current_iteration = 0;
 
-		while current_iteration2 < 10
+while current_iteration < 50%We have 50 elements
 
-			%Generate random position from the array of available positions -- That value in P will be the selected one
-			random_position = randi(length(available_positions), 1);
-			current_position = available_positions(random_position);%Get available position
-			available_positions = available_positions(available_positions ~= current_position);%Remove it from the array
-
-			%Get that position from the P and T arrays
-			current_element_P = P(:, (current_iteration * 10) + current_position);
-
-			current_position_T = mod(current_position,10);
-			if (current_position_T == 0)
-				current_position_T = 10;
-			end
-			current_element_T = Perfect(:, current_position_T);
-
-			%Now we have the P element, and the corresponding T element, so let's insert them in the final P:
-			Pfinal = [Pfinal, current_element_P];
-			Tfinal = [Tfinal, current_element_T];
-
-			current_iteration2 = current_iteration2 + 1;
-		end
-
-		current_iteration = current_iteration + 1;
+	%Get position in PerfectArial
+	current_position_T = mod(current_iteration,10);
+	if (current_position_T == 0)
+		current_position_T = 10;
 	end
 
-	% Increase cond
-	cond = cond + 1;
+	Pfinal = [Pfinal, P(:,current_iteration)];
+	Tfinal = [Tfinal, Perfect(:, current_position_T)];
+
+	current_iteration = current_iteration + 1;
 end
 
 save Pfinal;
