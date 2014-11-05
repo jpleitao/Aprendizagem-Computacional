c7b0bc7557439cb9e3fc7cd6800f5326be309120
jpleitao <jocaleitao93@gmail.com>
2014-11-05 16:48:08 +0000

    Created file with function to create our neural networks

---
+++ TP3/createNetwork.m
@@ -0,0 +1,15 @@
+%%%%
+%%
+%%  João Tiago Márcia do Nascimento Fernandes   2011162899
+%%  Joaquim Pedro Bento Gonçalves Pratas Leitão 2011150072
+%%
+%%%%
+function my_network = createNetwork(network_data)
+
+	%%%%
+	%%	network_data = [handles.networkName, handles.trainFunction, handles.performanceFunction, handles.goal, handles.epochs,
+	%%					handles.learningRate, handles.hiddenLayers, handles.layerDelays];
+	%%%%
+
+
+end
