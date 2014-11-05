24795aced561c5ef06a3d3c58b9086e73d91d76a
jpleitao <jocaleitao93@gmail.com>
2014-11-05 16:00:14 +0000

    Added our custom header, common to all the files

--- TP3/getPercentageData.m
+++ TP3/getPercentageData.m
@@ -1,6 +1,12 @@
+%%%%
+%%
+%%  João Tiago Márcia do Nascimento Fernandes   2011162899
+%%  Joaquim Pedro Bento Gonçalves Pratas Leitão 2011150072
+%%
+%%%%
 function [data_input, data_output] = getPercentageData(crysis_indexes, Target, Input, percentage)
 
-%randperm
+	%randperm
 	%divideind
 
 	%%%%
