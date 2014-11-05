7d64b65a27a93dddb1a2c392caf522fcfe38e3e0
jpleitao <jocaleitao93@gmail.com>
2014-11-05 15:21:33 +0000

    Separated the generation of the training and testing data, and finished first version of the generation of the training data, which by the way, should be very buggy XD

---
+++ TP3/getTestData.m
@@ -0,0 +1,12 @@
+%%%%
+%%
+%%  João Tiago Márcia do Nascimento Fernandes   2011162899
+%%  Joaquim Pedro Bento Gonçalves Pratas Leitão 2011150072
+%%
+%%%%
+function [test_input, test_output] = getTestData()
+
+
+	test_input = [];
+	test_output = [];
+end
