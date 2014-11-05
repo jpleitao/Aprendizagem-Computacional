983877ad5bfd139997aafb09df33086d2064d15e
jpleitao <jocaleitao93@gmail.com>
2014-11-05 15:20:06 +0000

    Changes to the GUI

--- TP3/run.m
+++ TP3/run.m
@@ -28,7 +28,7 @@ function varargout = run(varargin)
 
 % Edit the above text to modify the response to help run
 
-% Last Modified by GUIDE v2.5 05-Nov-2014 01:37:37
+% Last Modified by GUIDE v2.5 05-Nov-2014 12:21:33
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -68,7 +68,17 @@ handles.output = hObject;
 %%  Initialize all the variables we are going to use
 %%%%
 handles.training_file = '44202.mat';
+handles.test_file = '44202.mat';
 handles.percentage_training = 70;
+handles.percentage_test = 100 - handles.percentage_training;
+handles.goal = 1e-6;
+handles.epochs = 100;
+handles.trainFunction = 'trainlm';
+handles.performanceFunction = 'mse';
+handles.learningRate = 0.5;
+handles.hiddenLayers = 30;
+handles.layerDelays = 3;
+
 handles.training_input = [];
 handles.training_output = [];
 handles.test_input = [];
@@ -118,7 +128,9 @@ function trainbutton_Callback(hObject, eventdata, handles)
     crysis_indexes = getCrysisIndexes(Trg);
     
     %Get train and test data
-    [handles.training_input, handles.training_output, handles.test_input, handles.test_output] = getTrainingAndTestingData(crysis_indexes, Trg, FeatVectSel, handles.percentage_training);
+    
+    [handles.training_input, handles.training_output] = getTrainingData(crysis_indexes, Trg, FeatVectSel, handles.percentage_training);
+    [handles.test_input, handles.test_output] = getTestData();    
     
     %Create desired network(Shouldn't be needed, should already have been created)
     %handles.network
@@ -181,6 +193,7 @@ function percentageCrysisTrainInput_Callback(hObject, eventdata, handles)
 %        str2double(get(hObject,'String')) returns contents of percentageCrysisTrainInput as a double
 
     handles.percentage_training = str2double(get(hObject,'String'));
+    handles.percentage_test = 100 - handles.percentage_training;
 
 
 % --- Executes during object creation, after setting all properties.
@@ -210,9 +223,202 @@ function neuralNetworkTypePopUp_Callback(hObject, eventdata, handles)
 function neuralNetworkTypePopUp_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to neuralNetworkTypePopUp (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handstr2double(get(hObject,'String')) les not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in testDataListBox.
+function testDataListBox_Callback(hObject, eventdata, handles)
+% hObject    handle to testDataListBox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns testDataListBox contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from testDataListBox
+    contents = cellstr(get(hObject,'String'));
+    handles.test_file = contents{get(hObject,'Value')};
+
+% --- Executes during object creation, after setting all properties.
+function testDataListBox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to testDataListBox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function goalTextField_Callback(hObject, eventdata, handles)
+% hObject    handle to goalTextField (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of goalTextField as text
+%        str2double(get(hObject,'String')) returns contents of goalTextField as a double
+	handles.goal = str2double(get(hObject,'String'));
+    handles.goal
+
+
+% --- Executes during object creation, after setting all properties.
+function goalTextField_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to goalTextField (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function epochsInput_Callback(hObject, eventdata, handles)
+% hObject    handle to epochsInput (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of epochsInput as text
+%        str2double(get(hObject,'String')) returns contents of epochsInput as a double
+    handles.epochs = str2double(get(hObject,'String'));
+
+
+% --- Executes during object creation, after setting all properties.
+function epochsInput_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to epochsInput (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in trainFunctionsList.
+function trainFunctionsList_Callback(hObject, eventdata, handles)
+% hObject    handle to trainFunctionsList (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns trainFunctionsList contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from trainFunctionsList
+    contents = cellstr(get(hObject,'String'));
+    handles.trainFunction = contents{get(hObject,'Value')};
+
+
+% --- Executes during object creation, after setting all properties.
+function trainFunctionsList_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to trainFunctionsList (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
 % Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in performanceList.
+function performanceList_Callback(hObject, eventdata, handles)
+% hObject    handle to performanceList (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns performanceList contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from performanceList
+    contents = cellstr(get(hObject,'String'));
+    handles.performanceFunction = contents{get(hObject,'Value')};
+
+
+% --- Executes during object creation, after setting all properties.
+function performanceList_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to performanceList (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function learningRateEdit_Callback(hObject, eventdata, handles)
+% hObject    handle to learningRateEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of learningRateEdit as text
+%        str2double(get(hObject,'String')) returns contents of learningRateEdit as a double
+    handles.learningRate = str2double(get(hObject,'String'));
+
+% --- Executes during object creation, after setting all properties.
+function learningRateEdit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to learningRateEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function hiddenLayersEdit_Callback(hObject, eventdata, handles)
+% hObject    handle to hiddenLayersEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of hiddenLayersEdit as text
+%        str2double(get(hObject,'String')) returns contents of hiddenLayersEdit as a double
+    handles.hiddenLayers = str2double(get(hObject,'String'));
+
+
+% --- Executes during object creation, after setting all properties.
+function hiddenLayersEdit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to hiddenLayersEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function layerDelaysEdit_Callback(hObject, eventdata, handles)
+% hObject    handle to layerDelaysEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of layerDelaysEdit as text
+%        str2double(get(hObject,'String')) returns contents of layerDelaysEdit as a double
+    handles.layerDelays = str2double(get(hObject,'String'));
+
+% --- Executes during object creation, after setting all properties.
+function layerDelaysEdit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to layerDelaysEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
