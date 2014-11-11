2ef0963255a22e9e6ce4d3f2b1222eccdd896a5f
jpleitao <jocaleitao93@gmail.com>
2014-11-11 10:08:35 +0000

    More changes

--- TP3/getTestInputOutput.m
+++ TP3/getTestInputOutput.m
@@ -7,3 +7,14 @@ handles = struct('percentage_training', percentage_training, 'training_file', tr
 [training_input, training_output, test_input, test_output] = prepareDataSets(handles);
 
 save(output_training_file, 'test_input', 'test_output');
+
+
+training_file = '63502.mat';
+output_training_file = '63502_train30.mat';
+percentage_training = 30;
+
+handles = struct('percentage_training', percentage_training, 'training_file', training_file);
+
+[training_input, training_output, test_input, test_output] = prepareDataSets(handles);
+
+save(output_training_file, 'test_input', 'test_output');
