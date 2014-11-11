873599e285d233d94298e9a4a73944f849149a20
jpleitao <jocaleitao93@gmail.com>
2014-11-11 09:38:10 +0000

    More stuff for the tests

---
+++ TP3/getTestInputOutput.m
@@ -0,0 +1,9 @@
+training_file = '44202.mat';
+output_training_file = '44202_train30.mat';
+percentage_training = 30;
+
+handles = struct('percentage_training', percentage_training, 'training_file', training_file);
+
+[training_input, training_output, test_input, test_output] = prepareDataSets(handles);
+
+save(output_training_file, 'test_input', 'test_output');
