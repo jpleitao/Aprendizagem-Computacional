28907f2cf08a6b4dbcee9e327fc8a400774e2123
jpleitao <jocaleitao93@gmail.com>
2014-11-12 19:59:11 +0000

    Documented the convertResults.m file

--- TP3/convertResults.m
+++ TP3/convertResults.m
@@ -3,6 +3,13 @@
 %%  João Tiago Márcia do Nascimento Fernandes   2011162899
 %%  Joaquim Pedro Bento Gonçalves Pratas Leitão 2011150072
 %%
+%%  This functions takes the results of the execution of a given neural network, which is a 2-by-N array of float values, correponding to the
+%%  neural network's output.
+%%  The results of the classifications of our networks have to be one of two values:
+%%      - Ictal, represented by the array [0;1]
+%%      - Non-ictal, represented by the array [1;0]
+%%  So, to support this requirement, in this function we convert the network's output, which can be something like [0.5; 1.1], to the
+%%  required binary values, 0 or 1. This can be accomplish simply by setting to 1all values equal or higher to 0.5, and all the others to 0.
 %%%%
 function results = convertResults(results)
     
