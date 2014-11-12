830b3329e8064ae5b8a5a903c058207fef7199c5
jpleitao <jocaleitao93@gmail.com>
2014-11-12 20:08:38 +0000

    Finished documenting the translateOutputToGroup.m file

--- TP3/translateOutputToGroup.m
+++ TP3/translateOutputToGroup.m
@@ -3,15 +3,18 @@
 %%  João Tiago Márcia do Nascimento Fernandes   2011162899
 %%  Joaquim Pedro Bento Gonçalves Pratas Leitão 2011150072
 %%
+%%  This functions takes the neural network's filtered output, or the target output for a given test case, applies our group classification
+%%  approach. Our group classification approach consists simply in considering groups of data of a fixed size (defined in the "window_size"
+%%  variable) and counting the number of ictal classifications present in that group (ictal classifications are classifications equal to
+%%  [0;1]). If that number is equal or higher to a given threshold, then we consider the classification of that group to correspond to an
+%%  ictal situation, and is marked with the value "1". Otherwise, we consider a non-ictal situation, and is marked with the value "0".
+%%  Because we may not have a perfect classification, we have also included the detection of invalid classifications: Remember that we only
+%%  have two possible classifications: [0;1] for ictal situations and [1;0] for non-ictal situations. Any other classification different from
+%%  that, like [0;0] or [1;1], are considered invalid, and marked with the value "-1".
 %%%%
 function groupedOutput = translateOutputToGroup(output, groupLimitOnes, window_size)
 
     groupedOutput = [];
-    
-    %{
-        numberOnes = sum(output(i:i+window_size-1) == 1);
-        groupedOutput(i) = numberOnes >= groupLimitOnes;
-    %}
     
     size_output = size(output);
     number_cols = size_output(2);
