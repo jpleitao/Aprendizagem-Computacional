d1cbb0ac3f4f7dce105152df96af6fc8ba6f7f73
jpleitao <jocaleitao93@gmail.com>
2014-11-13 15:53:20 +0000

    stuff

--- TP3/new_test_stuff.m
+++ TP3/new_test_stuff.m
@@ -50,6 +50,110 @@ M = [specificity, sensitivity, true_positives, true_negatives, false_positives, 
 dlmwrite('test_results.csv',M,'delimiter',',', '-append');
 
 
+load('44202_train50.mat');
+
+load('trainedNetworks/net_Radial Basis Function_3.mat');
+
+network_results = sim(network, test_input);
+network_results = convertResults(network_results);
+
+expected_output = translateOutputToGroup(test_output, groupLimitOnes, window_size);
+got_output = translateOutputToGroup(network_results, groupLimitOnes, window_size);
+
+[true_positives, true_negatives, false_positives, false_negatives, invalid_data, expected_positives, expected_negatives] = interpretGroupedResults(expected_output, got_output);
+
+sensitivity = true_positives / (true_positives + false_negatives);
+specificity = true_negatives / (true_negatives + false_positives);
+
+M = [specificity, sensitivity, true_positives, true_negatives, false_positives, false_negatives, invalid_data];
+dlmwrite('test_results.csv',M,'delimiter',',', '-append');
+
+load('trainedNetworks/net_Radial Basis Function_7.mat');
+
+network_results = sim(network, test_input);
+network_results = convertResults(network_results);
+
+expected_output = translateOutputToGroup(test_output, groupLimitOnes, window_size);
+got_output = translateOutputToGroup(network_results, groupLimitOnes, window_size);
+
+[true_positives, true_negatives, false_positives, false_negatives, invalid_data, expected_positives, expected_negatives] = interpretGroupedResults(expected_output, got_output);
+
+sensitivity = true_positives / (true_positives + false_negatives);
+specificity = true_negatives / (true_negatives + false_positives);
+
+M = [specificity, sensitivity, true_positives, true_negatives, false_positives, false_negatives, invalid_data];
+dlmwrite('test_results.csv',M,'delimiter',',', '-append');
+
+
+load('trainedNetworks/net_Radial Basis Function_15.mat');
+
+network_results = sim(network, test_input);
+network_results = convertResults(network_results);
+
+expected_output = translateOutputToGroup(test_output, groupLimitOnes, window_size);
+got_output = translateOutputToGroup(network_results, groupLimitOnes, window_size);
+
+[true_positives, true_negatives, false_positives, false_negatives, invalid_data, expected_positives, expected_negatives] = interpretGroupedResults(expected_output, got_output);
+
+sensitivity = true_positives / (true_positives + false_negatives);
+specificity = true_negatives / (true_negatives + false_positives);
+
+M = [specificity, sensitivity, true_positives, true_negatives, false_positives, false_negatives, invalid_data];
+dlmwrite('test_results.csv',M,'delimiter',',', '-append');
+
+
+load('44202_train70.mat');
+
+load('trainedNetworks/net_Radial Basis Function_3.mat');
+
+network_results = sim(network, test_input);
+network_results = convertResults(network_results);
+
+expected_output = translateOutputToGroup(test_output, groupLimitOnes, window_size);
+got_output = translateOutputToGroup(network_results, groupLimitOnes, window_size);
+
+[true_positives, true_negatives, false_positives, false_negatives, invalid_data, expected_positives, expected_negatives] = interpretGroupedResults(expected_output, got_output);
+
+sensitivity = true_positives / (true_positives + false_negatives);
+specificity = true_negatives / (true_negatives + false_positives);
+
+M = [specificity, sensitivity, true_positives, true_negatives, false_positives, false_negatives, invalid_data];
+dlmwrite('test_results.csv',M,'delimiter',',', '-append');
+
+load('trainedNetworks/net_Radial Basis Function_7.mat');
+
+network_results = sim(network, test_input);
+network_results = convertResults(network_results);
+
+expected_output = translateOutputToGroup(test_output, groupLimitOnes, window_size);
+got_output = translateOutputToGroup(network_results, groupLimitOnes, window_size);
+
+[true_positives, true_negatives, false_positives, false_negatives, invalid_data, expected_positives, expected_negatives] = interpretGroupedResults(expected_output, got_output);
+
+sensitivity = true_positives / (true_positives + false_negatives);
+specificity = true_negatives / (true_negatives + false_positives);
+
+M = [specificity, sensitivity, true_positives, true_negatives, false_positives, false_negatives, invalid_data];
+dlmwrite('test_results.csv',M,'delimiter',',', '-append');
+
+
+load('trainedNetworks/net_Radial Basis Function_15.mat');
+
+network_results = sim(network, test_input);
+network_results = convertResults(network_results);
+
+expected_output = translateOutputToGroup(test_output, groupLimitOnes, window_size);
+got_output = translateOutputToGroup(network_results, groupLimitOnes, window_size);
+
+[true_positives, true_negatives, false_positives, false_negatives, invalid_data, expected_positives, expected_negatives] = interpretGroupedResults(expected_output, got_output);
+
+sensitivity = true_positives / (true_positives + false_negatives);
+specificity = true_negatives / (true_negatives + false_positives);
+
+M = [specificity, sensitivity, true_positives, true_negatives, false_positives, false_negatives, invalid_data];
+dlmwrite('test_results.csv',M,'delimiter',',', '-append');
+
+
 %{
 load('44202_train30.mat');
 
