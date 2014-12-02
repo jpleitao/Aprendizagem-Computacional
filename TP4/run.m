5ee1838f169e65f8af7045b48e7c5242be469f8a
jpleitao <jocaleitao93@gmail.com>
2014-12-02 15:29:24 +0000

    Updated run.m

---
+++ TP4/run.m
@@ -0,0 +1,5 @@
+%Read mamdini
+mamdani = readfis('mamdani.fis');
+
+%Open Model
+uiopen('/home/joaquim/Documents/Universidade/Engenharia Informática/4º Ano/1º Semestre/AC/PL1/Aprendizagem-Computacional/TP4/model.slx',1)
