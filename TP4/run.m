07868ba50c5a76d79c35004e92fa36028f96e4e2
jpleitao <jocaleitao93@gmail.com>
2014-12-04 01:56:45 +0000

    Corrected the implementations of the mamdani's controllers: Used Matlab R2014b for this, but left the 2014a version, which needs to be changed!

--- TP4/run.m
+++ TP4/run.m
@@ -1,5 +1,9 @@
 %Read mamdini
-mamdani = readfis('mamdani.fis');
+mamdani_9 = readfis('mamdani_9.fis');
+
+mamdani_25 = readfis('mamdani_25.fis');
 
 %Open Model
-uiopen('model.slx',1)
+uiopen('model_mamdani_9.slx',1)
+
+uiopen('model_mamdani_25.slx',1)
